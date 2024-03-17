f1e638640ce6e6ba9914c9cb0c28caf2819ef70e
Valan-Tamil-Dasan <valantamildasan@gmail.com>
2024-03-17 08:59:25 +0530

    updated port settings

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -21,4 +22,6 @@ def predict():
    
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # if 'PORT' env var is not found, default to 5000
+    
+    app.run(host='0.0.0.0', port=port)
